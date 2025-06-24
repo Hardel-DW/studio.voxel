c25c041af38113ee81ba88450fbfacef5f9c334b
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-06-24 11:00:15 +0200

    chore: Update @voxelio/breeze to version 1.2.15 in package.json and pnpm-lock.yaml

---
+++ src/components/tools/section/loot/Main.tsx
@@ -0,0 +1,31 @@
+import Donation from "@/components/tools/elements/Donation";
+import ToolSection from "@/components/tools/elements/ToolSection";
+import React from "react";
+
+export default function LootMainSection() {
+    return (
+        <ToolSection id="main" title={{ key: "tools.enchantments.section.global.description" }}>
+            <Donation
+                key="donation"
+                icon="/icons/logo.svg"
+                title={{ key: "tools.supports.title" }}
+                description={{ key: "tools.supports.description" }}
+                subTitle={{ key: "tools.supports.advantages" }}
+                extra={[
+                    { key: "tools.supports.advantages.early_access" },
+                    { key: "tools.supports.advantages.submit_ideas" },
+                    { key: "tools.supports.advantages.discord_role" },
+                    { key: "tools.supports.advantages.live_voxel" }
+                ]}
+                patreon={{
+                    text: { key: "tools.supports.become" },
+                    link: "https://www.patreon.com/hardel"
+                }}
+                tipText={{
+                    text: { key: "dialog.footer.donate" },
+                    link: "https://streamelements.com/hardoudou/tip"
+                }}
+            />
+        </ToolSection>
+    );
+}
