99befe50f9f6dd9610c9c39824355d2890c9e81e
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-10-09 13:37:24 +0200

    fix: fix api import

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@/lib/api";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(_: VercelRequest, res: VercelResponse) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
