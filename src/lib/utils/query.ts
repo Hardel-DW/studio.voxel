2203c60eadf0c84e55c7d63a5bfcf5fb91122d12
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-11-05 20:13:49 +0100

    Removed @tanstack/react-query-persist-client

--- src/lib/utils/query.ts
+++ src/lib/utils/query.ts
@@ -1,6 +1,4 @@
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { defaultShouldDehydrateQuery, isServer, QueryClient } from "@tanstack/react-query";
-import { persistQueryClient } from "@tanstack/react-query-persist-client";
 
 function makeQueryClient() {
     return new QueryClient({
@@ -20,19 +18,6 @@ let browserQueryClient: QueryClient | undefined;
 
 export function getQueryClient() {
     if (isServer) return makeQueryClient();
-
-    if (!browserQueryClient) {
-        browserQueryClient = makeQueryClient();
-
-        if (typeof window !== "undefined") {
-            const localStoragePersister = createSyncStoragePersister({ storage: window.localStorage });
-            persistQueryClient({
-                queryClient: browserQueryClient,
-                persister: localStoragePersister,
-                maxAge: 1000 * 60 * 60 * 24
-            });
-        }
-    }
-
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
 }
