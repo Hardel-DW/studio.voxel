e8550d56a1ed9f0572961a95a6e1e4b93d8d5e76
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-12-03 15:57:40 +0100

    Remove react scan in production

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import reactScan from './src/lib/scan'
 
 export default defineConfig({
     plugins: [
+        reactScan(),
         tailwindcss(),
         tanstackRouter({ target: 'react', autoCodeSplitting: true }),
         react({
