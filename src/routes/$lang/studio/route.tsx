1ac393da20082fcf7e3c7e56c203395c4102bcc1
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-12-18 23:16:50 +0100

    Refactor StudioLayout component to simplify light effect handling and add a grid background for enhanced visual structure.

--- src/routes/$lang/studio/route.tsx
+++ src/routes/$lang/studio/route.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { Fragment } from "react";
 import QueryProvider from "@/components/QueryProvider";
 import DebugPanel from "@/components/tools/debug/DebugPanel";
 import { FloatingBarProvider } from "@/components/tools/floatingbar/FloatingBarContext";
@@ -12,18 +11,28 @@ export const Route = createFileRoute("/$lang/studio")({
 
 function StudioLayout() {
     const [disableEffects] = useLocalStorage("studio:disable-effects", false);
-    const [disableLight] = useLocalStorage("studio:disable-light", false);
 
     return (
         <div className="flex relative h-dvh">
-            {disableLight && (
-                <Fragment>
-                    <div className="fixed -z-50 -top-16 -right-16 size-72 rounded-full blur-3xl bg-linear-to-br from-red-900/20 to-blue-900/20" />
-                    <div className="fixed -z-50 top-0 bottom-0 translate-y-1/2 -left-8 w-64 h-full rounded-full blur-3xl bg-linear-to-br from-pink-900/20 to-blue-900/20" />
-                    <div className="fixed -z-50 -bottom-24 -right-24 size-60 rounded-full blur-3xl bg-linear-to-br from-purple-900/20 to-red-900/20" />
-                    <div className="fixed -z-50 -top-16 -left-16 size-100 rounded-full blur-3xl bg-linear-to-br from-pink-900/20 to-blue-900/20" />
-                </Fragment>
-            )}
+            <div className="fixed -z-50 -top-16 -right-16 size-72 rounded-full blur-3xl bg-linear-to-br from-red-900/20 to-blue-900/20" />
+            <div className="fixed -z-50 top-0 bottom-0 translate-y-1/2 -left-8 w-64 h-full rounded-full blur-3xl bg-linear-to-br from-pink-900/20 to-blue-900/20" />
+            <div className="fixed -z-50 -bottom-24 -right-24 size-60 rounded-full blur-3xl bg-linear-to-br from-purple-900/20 to-red-900/20" />
+            <div className="fixed -z-50 -top-16 -left-16 size-100 rounded-full blur-3xl bg-linear-to-br from-pink-900/20 to-blue-900/20" />
+
+
+            <div className="-z-10 absolute inset-0 overflow-hidden">
+                <svg
+                    className="size-full stroke-white/15 [stroke-dasharray:5_6] [stroke-dashoffset:10] stroke-2 scale-110"
+                    style={{ transform: "skewY(-12deg)" }}>
+                    <title>Grid</title>
+                    <defs>
+                        <pattern id="grid" viewBox="0 0 64 64" width="32" height="32" patternUnits="userSpaceOnUse" x="0" y="0">
+                            <path d="M64 0H0V64" fill="none" />
+                        </pattern>
+                    </defs>
+                    <rect width="100%" height="100%" fill="url(#grid)" />
+                </svg>
+            </div>
 
             {disableEffects && (
                 <div className="fixed inset-0 -z-10">
