963f23ed31a658f68a3170d2303624868148b642
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2026-01-06 15:40:30 +0100

    Update Change Store to use logger version to not recompile

--- src/lib/store/ChangesStore.ts
+++ src/lib/store/ChangesStore.ts
@@ -8,19 +8,24 @@ interface ChangesState {
     compiledFiles: Record<string, Uint8Array>;
     originalFiles: Record<string, Uint8Array>;
     diff: Map<string, FileStatus>;
+    lastLoggerVersion: number;
     compile: () => void;
 }
 
-export const useChangesStore = create<ChangesState>((set) => ({
+export const useChangesStore = create<ChangesState>((set, get) => ({
     compiledDatapack: null,
     compiledFiles: {},
     originalFiles: {},
     diff: new Map(),
+    lastLoggerVersion: -1,
     compile: () => {
-        const { files, compile } = useConfiguratorStore.getState();
+        const { files, compile, logger } = useConfiguratorStore.getState();
+        const currentVersion = logger?.getVersion() ?? 0;
+        if (get().lastLoggerVersion === currentVersion) return;
+
         const compiledDatapack = compile();
         const compiledFiles = compiledDatapack.getFiles();
         const diff = new DatapackDownloader(compiledFiles).getDiff(files);
-        set({ compiledDatapack, compiledFiles, originalFiles: files, diff });
+        set({ compiledDatapack, compiledFiles, originalFiles: files, diff, lastLoggerVersion: currentVersion });
     }
 }));
